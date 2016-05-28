905c78631d36bbf1c37daa54207c5e91e0584390
Lorenzo <tunedconsulting@gmail.com>
2016-05-28 17:34:47 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pip_tkinter',
+    version='0.0.1',
+    description='A Tk GUI for pip',
+    author='',
+    author_email='pip_tkinter@mailinglist.com',
+    url='',
+    packages=['tkinter', 'pip'],
+)
