507d518721b3a8151d00dd19231984e2be69af48
scorpiolegacy <upendra.k14@iiits.in>
2016-08-23 13:58:22 +0530

    Adds cron app

---
+++ pythonlibscronapp/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='PythonlibsCrawler',
+      version='1.0',
+      description='OpenShift App',
+      author='Upendra Kumar',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['beautifulsoup4','lxml'],
+     )
